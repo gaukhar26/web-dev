ac3c94a085f91a64750e87b6578b74ac48b839e2
gaukhar26 <123442198+gaukhar26@users.noreply.github.com>
2024-02-21 21:47:45 +0600

    Add files via upload

---
+++ lab4/shop/src/app/products.ts
@@ -0,0 +1,119 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  link: string;
+  rating: number;
+}
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: 'Kettle',
+    price: 799,
+    description: 'A great electronic teapot',
+    image:
+      'https://resources.cdn-kaspi.kz/img/m/p/h08/hde/80282292781086.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/elektrochainik-bereke-br-810-seryi-109981423/?c=750000000',
+    rating: 4,
+  },
+
+  {
+    id: 2,
+    name: 'Plates',
+    price: 200,
+    description: 'A great set of plates',
+    image:
+      'https://resources.cdn-kaspi.kz/img/m/p/h00/h66/64176040902686.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/emal-nabor-posudy-klubnika-sadovaja-ais-110t-emal-4-sht-103442013/?c=750000000',
+    rating: 5,
+  },
+  {
+    id: 3,
+    name: 'Wardrobe',
+    price: 1000,
+    description: 'A great wardrobe',
+    image:
+      'https://resources.cdn-kaspi.kz/img/m/p/h71/h15/64429190742046.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/sanfor-kapri-160-2x46-6x190-5-belyi-bezhevyi-106768828/?c=750000000',
+    rating: 4,
+  },
+  {
+    id: 4,
+    name: 'Iphone',
+    price: 900,
+    description: 'A great phone',
+    image:
+      'https://resources.cdn-kaspi.kz/img/m/p/h32/h70/84378448199710.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-midnight-chernyi-102298404/?c=750000000',
+    rating: 5,
+  },
+  {
+    id: 5,
+    name: 'Shampoo',
+    price: 100,
+    description: 'A great shampoo',
+    image:
+      'https://resources.cdn-kaspi.kz/img/m/p/hc2/he7/84923354873886.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/pantene-3-v-1-intensivnoe-vosstanovlenie-shampun-360-ml-100189794/?c=750000000',
+    rating: 4,
+  },
+  {
+    id: 6,
+    name: 'Toys',
+    price: 100,
+    description: 'A great set of toys',
+    image:
+      'https://resources.cdn-kaspi.kz/img/m/p/h68/h59/64130713354270.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/mjach-sobebear-taktil-nye-mjachiki-mul-tikolor-103435487/?c=750000000',
+    rating: 5,
+  },
+  {
+    id: 7,
+    name: 'Chocolate',
+    price: 80,
+    description: 'A great chocolate',
+    image:
+      'https://resources.cdn-kaspi.kz/img/m/p/h21/h90/70365637279774.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/rahat-shokoladnaja-plitka-molochnyi-100-g-100221859/?c=750000000',
+    rating: 5,
+  },
+  {
+    id: 8,
+    name: 'Coca Cola',
+    price: 60,
+    description: 'A great drink',
+    image:
+      'https://resources.cdn-kaspi.kz/img/m/p/h1c/hf1/85128198684702.png?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/coca-cola-classic-gazirovannyi-napitok-2-l-100208094/?c=750000000',
+    rating: 4,
+  },
+  {
+    id: 9,
+    name: 'Cups',
+    price: 190,
+    description: 'A great set of cups',
+    image:
+      'https://resources.cdn-kaspi.kz/img/m/p/h72/h2e/84929932066846.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/kruzhka-zarema-home-727-250-ml-6-sht-keramika-115931348/?c=750000000',
+    rating: 5,
+  },
+  {
+    id: 10,
+    name: 'Painting',
+    price: 500,
+    description: 'A great painting',
+    image:
+      'https://resources.cdn-kaspi.kz/img/m/p/h88/hac/64047075065886.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/paintboy-gx29450-50-sm-x-40-sm-104063487/?c=750000000',
+    rating: 4,
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
